7e1ec57948b53ea418a8663d355cae5d9e24cc5f
woomatrix <phppoet1986@gmail.com>
2023-12-08 17:27:24 +0530

    Create easy-checkout-file-upload.php

---
+++ easy-checkout-file-upload.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * File Upload module easy checkout field editor
+ *
+ * @author            SysBasics
+ * @copyright         2019 Your SysBasics
+ * @license           GPL-2.0-or-later
+ *
+ * @wordpress-plugin
+ * Plugin Name:       File Upload module easy checkout field editor
+ * Plugin URI:        https://sysbasics.com
+ * Description:       Description of the plugin.
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Your Name
+ * Author URI:        https://sysbasics.com
+ * Text Domain:       plugin-slug
+ * License:           GPL v2 or later
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Update URI:        https://example.com/my-plugin/
+ */
